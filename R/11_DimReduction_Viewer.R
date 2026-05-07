a64c8bdea0b3203fef176955b6e1caff87b8be9d
ETIENNE Cyril <cyril.etienne@cea.fr>
2026-05-07 09:53:58 +0200

    Updated plotDimReduction to allow plotting of specific samples in density plots

--- R/11_DimReduction_Viewer.R
+++ R/11_DimReduction_Viewer.R
@@ -54,16 +54,34 @@ plotDimReduction <- function(CYTdata,
   
   
   if (plotType == "density") {
-    if (!is.null(samples) || !is.null(population)) {
-      stop("Error : 'plotType' argument is set to 'density' and requires the entire dataset to be computed ('samples' and 'population' arguments must both be set to NULL)")
+    # if (!is.null(samples) || !is.null(population)) {
+    #   stop("Error : 'plotType' argument is set to 'density' and requires the entire dataset to be computed ('samples' and 'population' arguments must both be set to NULL)")
+    # }
+    
+    #Changed here 07-05-2026
+    samples = checkorderSamples(CYTdata, samples, order=TRUE, checkDuplicates=TRUE)
+    data = cbind.data.frame(data, "spl" = CYTdata@samples)
+    colorIndexes = (data$spl %in% samples)
+    dataGrey = subset(data, !colorIndexes)
+    data = subset(data, colorIndexes)
+    
+    plot = ggplot2::ggplot()
+    
+    if (nrow(dataGrey)>0) {
+      plot = plot +
+        ggplot2::geom_point(data = dataGrey,
+                            ggplot2::aes_string(x = colnames(dataGrey)[1],
+                                                y = colnames(dataGrey)[2]),
+                            color = "gray", size = 0.001) +
+        ggnewscale::new_scale_color()
     }
     
-    colnames(data) = c("x", "y") 
-    plot <- ggplot2::ggplot(data, ggplot2::aes(x,y)) +
-      ggpointdensity::geom_pointdensity(size=0.001) +
+    plot <- plot + 
+      ggpointdensity::geom_pointdensity(data, 
+                                        ggplot2::aes(x,y),
+                                        size=0.001) +
       viridis::scale_color_viridis() +
       ggplot2::labs(color = "Cell density")
-    legendPosition = "right"
   }
   else {
     plot <- ggplot2::ggplot()
@@ -103,7 +121,7 @@ plotDimReduction <- function(CYTdata,
                                                 color = "metadata"),
                             size = 0.001) +
         ggplot2::guides(colour = ggplot2::guide_legend(override.aes = list(alpha = 1, size = 2), ncol = 1, title = metadata))
-      legendPosition = "right"
+      # legendPosition = "right"
     }
   }
   
